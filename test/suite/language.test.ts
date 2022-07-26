bf9303845f2a35b992784eedc5c47656b1217a9c
Nicolas Hedger <nicolas@hedger.ch>
2022-07-26 21:15:10 +0200

    chore: add timeout

--- test/suite/language.test.ts
+++ test/suite/language.test.ts
@@ -5,6 +5,7 @@ const openTestSchemaInEditor = async (
     content: string,
     selection?: Selection
 ): Promise<TextEditor> => {
+
     const document = await workspace.openTextDocument({
         language: "zed",
         content: content,
@@ -17,18 +18,17 @@ const openTestSchemaInEditor = async (
     return editor;
 };
 
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
 suite("Language Configuration", async () => {
     test("it supports toggling line comments", async () => {
         const editor = await openTestSchemaInEditor(
             "comment me please",
             new Selection(0, 0, 0, 0)
         );
-
         await commands.executeCommand("editor.action.commentLine");
-
+        await sleep(2000);
         assert.equal(editor.document.getText(), "// comment me please");
-
-        await commands.executeCommand("workbench.action.closeActiveEditor");
     });
 
     test("it supports toggling block comments", async () => {
@@ -36,11 +36,8 @@ suite("Language Configuration", async () => {
             "comment me please",
             new Selection(0, 0, 0, 17)
         );
-
         await commands.executeCommand("editor.action.blockComment");
-
+        await sleep(2000);
         assert.equal(editor.document.getText(), "/* comment me please */");
-
-        await commands.executeCommand("workbench.action.closeActiveEditor");
     });
 });
